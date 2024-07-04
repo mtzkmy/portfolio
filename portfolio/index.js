5179528cd7d91304c1f8d3a1b15db9f2c6ca447b
mtzkmy <162808962+mtzkmy@users.noreply.github.com>
2024-07-04 23:02:55 +0900

    Add files via upload

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,7 +1,18 @@
 // 監視対象が範囲に現れたら実行する動作
-const showsky = () => {
-  console.log('あおぞら');
+const showsky = (entries) => {
+  const keyframes = {
+    opacity: [0, 1],
+    translate: ['0 0', 0],
+  }
+entries[0].target.animate(keyframes, 3000);
+
+
 };
 
 // 監視の設定
 const skyObserver = new IntersectionObserver(showsky);
+
+// skyを監視するように指示
+skyObserver.observe(document.querySelector('#sky'));
+
+
